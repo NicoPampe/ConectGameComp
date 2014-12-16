f50ff13908173f46a7ad59b81ba960e670432743
Nico Pampe <npampe@mines.edu>
2014-12-15 21:15:05 -0700

    Fixed the keyPressed and KeyRelease events for pratice

--- source/main.cpp
+++ source/main.cpp
@@ -36,6 +36,10 @@ int main() {
 	// run the program as long as the window is open
 	while (window.isOpen())
 	{
+		// call clear at the start of the loop
+		window.clear(sf::Color::Black);
+
+
 		// creat an event, check all of the windows events
 		sf::Event event;
 		while (window.pollEvent(event))
@@ -43,43 +47,13 @@ int main() {
 			// "close ruquest" we want to close the window and program
 			if (event.type == sf::Event::Closed || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
 				window.close();
-
-			// "close ruquest" we want to close the window and program
-			if (event.type == sf::Event::Closed || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
-				window.close();
-			/*else if (event.KeyPressed)
+			if (event.key.code == sf::Keyboard::Space && event.type == sf::Event::KeyPressed)
 			{
-			std::cout << "key" << std::endl;
-			break;
-			}*/
-			if (event.type == sf::Event::TextEntered)
-			{
-				if (event.text.unicode < 120)
-					std::cout << "ASCII character typed is: " << static_cast<char>(event.text.unicode) << std::endl;
+				//window.setKeyRepeatEnabled(false);
+				rectangle.rotate(90);
 			}
-			if (event.type == sf::Event::KeyPressed)
-			{
-				if (event.key.code == sf::Keyboard::L)
-				{
-					std::cout << "the escape key was pressed" << std::endl;
-					std::cout << "control:" << event.key.control << std::endl;
-					std::cout << "alt:" << event.key.alt << std::endl;
-					std::cout << "shift:" << event.key.shift << std::endl;
-					std::cout << "system:" << event.key.system << std::endl;
-				}
-			}
-			if (event.type == sf::Event::MouseMoved)
-			{
-				//std::cout << "new mouse x: " << event.mouseMove.x << std::endl;
-				//std::cout << "new mouse y: " << event.mouseMove.y << std::endl;
-			}
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-				std::cout << "Left key was pressed" << std::endl;
-			else
-				break;
 		}
 
-		window.clear(sf::Color::Black);
 
 		sf::CircleShape shape(50);
 
@@ -96,11 +70,7 @@ int main() {
 			rectangle.move(0, 5);
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
 			rectangle.move(0, -5);
-		if (event.key.code == sf::Keyboard::Space)
-		{
-			window.setKeyRepeatEnabled(false);
-			rectangle.rotate(90);
-		}
+		
 		window.draw(rectangle);
 
 		window.display();
