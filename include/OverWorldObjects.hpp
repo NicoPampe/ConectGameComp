cad68d7fc2d3ea35dc47ef4f0faf5c3ffecd16df
Nico Pampe <npampe@mines.edu>
2014-12-17 14:56:22 -0700

    We added lua to the project. Lua is a programing language. It will help us load and read maps.

--- include/OverWorldObjects.hpp
+++ include/OverWorldObjects.hpp
@@ -6,13 +6,7 @@
 class Tree {
 public:
 	sf::Sprite tree_;
-
-	int get_tree_position_x_() { return tree_position_.x; };
-	int get_tree_position_y_() { return tree_position_.y; };
-
-	sf::Vector2f min_bounds = sf::Vector2f(tree_position_.x, tree_position_.y);
-	sf::Vector2f max_bounds = sf::Vector2f(tree_position_.x + 59, tree_position_.y + 80);
-
+	
 	void set_tree_position_(sf::Vector2f position_) { tree_.setPosition(position_); tree_position_ = position_; };
 	void set_texture_() { tree_texture_.loadFromFile("Tree1.png"); tree_.setTexture(tree_texture_); };
 	//sf::Sprite get_sprite() { return tree_; };
