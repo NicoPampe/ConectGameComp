2406647f3f5cc0d232aaa952389aace6f9d59510
Nico Pampe <npampe@mines.edu>
2014-12-16 17:26:26 -0700

    Change the OverWorld file. No resizing should be ocuring

--- source/OverWorld.cpp
+++ source/OverWorld.cpp
@@ -14,7 +14,7 @@ void OverWorld::DrawOverWorld(sf::RenderWindow &window) {
 	sf::Vector2u windowScale = window.getSize();
 	//sf::Vector2f size = window.getSize();
 	//Background.setScale(windowScale.x, windowScale.y);
-	Background.setScale((float)1 / ((float)1000 / windowScale.x), (float)1 / ((float)1000 / windowScale.y));
+	//Background.setScale((float)1 / ((float)1000 / windowScale.x), (float)1 / ((float)1000 / windowScale.y));
 	//Background.setScale(.5, .5);
 	sf::Vector2f sizeB = Background.getScale();
 	std::cout << sizeB.x << " " << sizeB.y << std::endl;
