ffd0c043bcd6a74705ca6bd0d756d7d5291cd754
Chris Butler <chrisbutler296@gmail.com>
2014-12-17 11:25:32 -0700

    Bounds checking on Tree works.

--- source/OverWorld.cpp
+++ source/OverWorld.cpp
@@ -4,50 +4,49 @@
 
 void OverWorld::movePlayer(float dx, float dy) {
 	player_ += sf::Vector2f(dx, dy);
+	auto player_size = sf::Vector2f(20, 25);
 
-	// The player square is 20 units wide.
-	if (player_.x > max_bounds_.x - 20) {
-		player_.x = max_bounds_.x - 20;
+	if (player_.x > max_bounds_.x - player_size.x) {
+		player_.x = max_bounds_.x - player_size.x;
 	}
 	if (player_.x < min_bounds_.x) {
 		player_.x = min_bounds_.x;
 	}
 
-	// ... and 25 tall.
-	if (player_.y > max_bounds_.y - 25) {
-		player_.y = max_bounds_.y - 25;
+	if (player_.y > max_bounds_.y - player_size.y) {
+		player_.y = max_bounds_.y - player_size.y;
 	}
 	if (player_.y < min_bounds_.y) {
 		player_.y = min_bounds_.y;
 	}
 
-	// running into objects
-	std::cout << player_.x << " " <<player_.y << " : "  << tree1_.get_tree_position_x_() << " " << tree1_.get_tree_position_y_() << std::endl;
-	// Max bound 59 min bound 80
-	sf::FloatRect bounding_box = tree1_.tree_.getGlobalBounds();
-	if (bounding_box.contains(player_)) {
-		std::cout << "collision!   ";
-		if (player_.x < tree1_.get_tree_position_x_() + 59)
-			std::cout << "from the x";
+	sf::FloatRect tree_box = tree1_.tree_.getGlobalBounds();
+	auto player_box = sf::FloatRect(player_, sf::Vector2f(player_size.x, player_size.y));
+	if (tree_box.intersects(player_box)) {
+		if (dx > 0) {
+			// We're coming from the LEFT, but overshooting.
+			//   We need to move the player to the LEFT.
+			// [P ---]-- | -- x
+			player_.x = tree_box.left - player_size.x;
+		} else if (dx < 0) {
+			// We're coming from the RIGHT, but overshooting.
+			//   We need to move the player to the RIGHT.
+			// x --| --[P ---]
+			player_.x = tree_box.left + tree_box.width;
+		} else {
+			// dx == 0, there's nothing to do.
+		}
 
-		int delt_x = player_.x - tree1_.get_tree_position_x_();
-		int delt_y = player_.y - tree1_.get_tree_position_y_();
-		if (delt_x > delt_y)
-			player_.x = tree1_.get_tree_position_x_() + 59;
-		else if (delt_x < delt_y)
-			player_.y = tree1_.get_tree_position_y_() + 80;
-		std::cout << std::endl << delt_x << "  " << delt_y << std::endl;
-		
-		/*if (player_.x < tree1_.get_tree_position_x_() + 59)
-			player_.x = tree1_.get_tree_position_x_() + 59;
-		if (player_.y < tree1_.get_tree_position_y_() + 80)
-			player_.y = tree1_.get_tree_position_y_() + 80;*/
+		if (dy > 0) {
+			// Coming from ABOVE, move player UP. (lower y)
+			player_.y = tree_box.top - player_size.y;
+		} else if ( dy < 0) {
+			// Coming from BELOW, move player DOWN. (higher y)
+			player_.y = tree_box.top + tree_box.height;
+		} else {
+			// dy == 0, there's nothing to do.
+		}
 	}
-	
-	// TODO: fix the code to use the max and min bounds. should not be using the get_x and get_y. 
-	/*if (player_.x < tree1_.max_bounds.x) {
-		player_.x = tree1_.max_bounds.x;
-	}*/
 }
 
 sf::Vector2f OverWorld::playerPosition() {
