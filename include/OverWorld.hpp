b64e3c3670ffcec3df9045164b4abf732c1559ab
Nico Pampe <npampe@mines.edu>
2014-12-16 17:07:19 -0700

    Added a sprite for the overWorld. The sprite has a basic texture background.

--- include/OverWorld.hpp
+++ include/OverWorld.hpp
@@ -1,15 +1,17 @@
 #pragma once
 
+#include <SFML\Graphics.hpp>
 
 // This class is the world that the party can navagate in. The OverWorld will be a gride like layout that the party can move on, from square to square.
 // As they navagate the world, they should interacte with grides around them.
 
 class OverWorld {
 public:
-	OverWorld () {}
-
-
+	void SetBackground();
+	void DrawOverWorld(sf::RenderWindow &window);
+	
 private:
-
+	sf::Texture texture;
+	sf::Sprite Background;
 };
 
--- source/OverWorld.cpp
+++ source/OverWorld.cpp
@@ -1,1 +1,22 @@
 #include "OverWorld.hpp"
+
+
+#include <iostream>
+
+void OverWorld::SetBackground() {
+	//texture.loadFromFile("TestOverWorld.png");
+	if (!texture.loadFromFile("TestOverWorld.png"))
+		std::cout << "ERROR OPENING FILE";
+	Background.setTexture(texture);	
+}
+
+void OverWorld::DrawOverWorld(sf::RenderWindow &window) {
+	sf::Vector2u windowScale = window.getSize();
+	//sf::Vector2f size = window.getSize();
+	//Background.setScale(windowScale.x, windowScale.y);
+	Background.setScale((float)1 / ((float)1000 / windowScale.x), (float)1 / ((float)1000 / windowScale.y));
+	//Background.setScale(.5, .5);
+	sf::Vector2f sizeB = Background.getScale();
+	std::cout << sizeB.x << " " << sizeB.y << std::endl;
+	window.draw(Background);
+}
